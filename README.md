76a6ea49a20efbef69bb390338c7a31fe824ed06
Nick <nick.e.lambropoulos@gmail.com>
2018-02-22 15:33:53 +0200

    removed project files from documentation branch

--- README.md
+++
@@ -1,2 +0,0 @@
-# BigDataLabOne
-Data
--- setZero.h
+++
@@ -1,9 +0,0 @@
-#ifndef SETZERO_H
-#define  SETZERO_H
-#include "NumElements.h"
-void setZero(int* Array, size_t K, size_t Bounds[])
-{
-  for (int n = 0; n < NumElements(K,Bounds);n++)
-    Array[n] = 0;
-}
-#endif
